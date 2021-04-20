a8a90198985dd4539b15e1be22bceca8a15b335f
Wing hong Ho <w.ho@garvan.org.au>
2021-04-20 15:15:39 +1000

    initial commit

---
+++ genomics_r_basics.R
@@ -0,0 +1,113 @@
+#this line creates the object 'a' and assigns it the value '1'
+a<-1
+Total_human_Chromosomes<-23
+Gene_name<-'PYMT'
+URL<-'ftp://ftp.ensemblgenomes.org/pub/bacteria/release-39/fasta/bacteria_5_collection/escherichia_coli_b_str_rel606/'
+diploid_human_cell<-2*Total_human_Chromosome 
+rm(URL)
+rm(a)
+Gene_name
+URL
+rm(diploid_human_cell,Gene_name,Total_human_Chromosomes)
+chromosome_name <- 'chr02'
+od_600_value <- 0.47
+chr_position <- '1001701'
+spock <- TRUE
+pilot<-'Earhart'
+mode(chromosome_name)
+mode(od_600_value)
+mode(chr_position)
+mode(spock)
+mode(pilot)
+golden_ratio <- (1+sqrt(5))/2
+?round()
+round(golden_ratio, digits=3)
+snp_gene <- c('OXTR','ACTN3','AR','OPRM1')
+mode(snp_gene)
+lenght(snp_gene)
+length(snp_gene)
+text_collection <- c(1,2,'text')
+mode(text_collection)
+str(snp_gene)
+str(text_collection)
+length(text_collection)
+rm(text_collection)
+snp_gene <- c(snp_gene, 'CYP1A1','APOA5')
+snp_gene
+snp_gene[-1]
+snp_gene[3] <- ER
+snp_gene[3] <- 'ER'
+snp_positions[snp_positions > 100000000]
+snp_gene[snp_gene>3]
+snp_gene[3-6]
+snp_gene
+snp_gene[2]
+snp_gene[1,2,3]
+snp_gene[1][2]
+snp_gene
+snp_gene[c{snp_gene
+is.na(snp_gene)
+snp_gene <- c(snp_gene, NA)
+is.na(snp_gene)
+snp_gene[-7]
+snp_gene
+snp_gene[7] <- "APOA5"
+snp_gene
+snp_gene <- c(snp_gene,'CYP1A1','APOA5')
+snp_gene[-6]
+snp_gene
+snp_gene <- snp_gene[-6]
+snp_gene[7] <- 'APOA5'
+is.NA(snp_gene)
+is.na(snp_gene)
+c('ACTN3')%in%snp_gene
+c('actn3')%in%snp_gene
+typeof(combined)
+?typeof
+typeof(snp_gene)
+mode(snp_gene)
+snps <- c('rs53576', 'rs1815739', 'rs6152', 'rs1799971')
+snp_chromosome <- c('3','11','X','6')
+snp_positions <- c(8762685, 66560624, 67545785, 154039662)
+snp_gene[3]
+snps[1:3]
+snp_gene[c(1,3,4)]
+snp_gene
+snp_positions[snp_positions>10000000]
+snp_positions[snp_positions>100000000]
+snp_positions>100000000
+which(snp_positions>100000000)
+snp_marker_cutoff <-100000000
+snp_positions[snp_positions>snp_marker_cutoff]
+?write.csv
+data()
+datasets[women]
+?indometh
+?BOD
+my_dataset <- UKgas
+rm(my_dataset)
+my_dataset <- Indometh                                  
+typeof(my_dataset)
+str(my_dataset)
+length(my_dataset)
+my_dataset[c(1)]
+my_dataset[c(row=1)]
+?c()
+my_dataset[67] <- 'test'
+?read.csv
+?nrow
+nrow(my_dataset)
+ncol(my_dataset)
+nrow=10
+rm(nrow)
+install("readr")
+renv::install("readr")
+?readr
+??readr
+install.packages("BiocManager")
+?biocManager
+BiocManager::install("readr")
+readr
+renv::install("readr")
+dataset()
+my_dataset
